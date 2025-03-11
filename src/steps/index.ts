793041fcd1e8c5fc3cd80de0330fed99d53485fe
vs <valeriisklyarov@gmail.com>
2025-03-11 18:09:21 +0200

    feat: implement CAPTCHA handling with manual intervention and notification

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,5 +1,6 @@
-export * from './handleCaptcha.js';
+export * from './handleCaptchaManual.js';
 export * from './email-verification-handler.js';
 export * from './scrollAndCollectData.js';
 export * from './find-filters-elements.js';
 export * from './set-filters.js';
+export * from './handleCaptchaSolverApi.js';
